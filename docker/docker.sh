0608d21ce8271b321458c22edc8a600e5478f1ba
madmazoku <madmazoku@gmail.com>
2018-03-30 15:11:11 +0300

    fix docker

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,6 +1,6 @@
 echo ========= Build docker image
 docker build -t otus.lessons.28.01 .
 echo ========= Execute ymr
-docker run --rm -i otus.lessons.28.01 ./ymr ../data/test.in.txt 7 5
+docker run --rm -i otus.lessons.28.01 ymr test.in.txt 7 5
 echo ========= Remove docker image
 docker rmi otus.lessons.28.01
